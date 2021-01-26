0843f6c61bc86e4f6617c0d52070817dff69dc16
Александр Бондарь <ya.sasha.bondar30@gmail.com>
2021-01-26 13:47:20 +0300

    Added user table and changed user model

--- app/User.php
+++ app/User.php
@@ -3,12 +3,36 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
+
+    const TABLE_NAME = 'user';
+
+    const ATTR_ID              = 'id';
+    const ATTR_NAME            = 'name';
+    const ATTR_PHONE           = 'phone';
+    const ATTR_PHONE_VERIFY_AT = 'phone_verified_at';
+    const ATTR_EMAIL           = 'email';
+    const ATTR_EMAIL_VERIFY_AT = 'email_verify_at';
+    const ATTR_IMAGE           = 'image';
+    const ATTR_ROLE            = 'role';
+    const ATTR_PASSWORD        = 'password';
+    const ATTR_ADDRESS         = 'address';
+    const ATTR_REMEMBER_TOKEN  = 'remember_token';
+    const ATTR_CREATED_AT      = 'created_at';
+    const ATTR_UPDATED_AT      = 'updated_at';
+
+    const ROLE_ADMIN    = 2;
+    const ROLE_DELIVERY = 1;
+    const ROLE_USER     = 0;
+
+    protected $table = self::TABLE_NAME;
 
     /**
      * The attributes that are mass assignable.
@@ -17,6 +41,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'email', 'password',
+    ];
+
+    protected $guarded = [
+        'role'
     ];
 
     /**
@@ -35,5 +63,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime',
     ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('image')->nullable();
+            $table->string('email')->unique()->index();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique()->index();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->integer('role')->default(0); // 0 - пользователь
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
